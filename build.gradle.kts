542a8e7b7e6cb2f8ead75ee048bda92451e83702
Andrei.Tyrin <andrei.tyrin@jetbrains.com>
2025-08-28 20:28:18 +0200

    Bump KGP to 2.2.20-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "2.2.20-Beta2"
+    id("org.jetbrains.kotlin.jvm") version "2.2.20-RC"
     id("org.jetbrains.intellij.platform") version "2.7.2"
 }
 
